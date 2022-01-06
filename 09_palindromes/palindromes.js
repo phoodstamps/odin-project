173f3858ca63c4ac7815233e9339487cc7b9ba80
phoodstamps <estevez.ryan.c@gmail.com>
2022-01-06 17:21:26 -0500

    working on js fundamentals 5

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (stringVal) {
+  let reverseString = "";
+  let cleanString = "";
 
+  for (s in stringVal) {
+    if ((stringVal.charAt(s)).match(/[a-z]/i))
+      cleanString += stringVal.charAt(s);
+  }
+
+  for (i = cleanString.length-1; i >= 0; i--) {
+      reverseString += cleanString.charAt(i);
+  }
+  
+  if (reverseString.toLowerCase() === cleanString.toLowerCase())
+    return true;
+  else
+    return false;
+};
+
+// Solution from repo
+const palindromesSolution = function(string) {
+  processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
 };
 
 // Do not edit below this line
